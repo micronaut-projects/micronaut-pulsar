c72e31217cf2edbb65b3600ff025cd490bccfbe1
Haris Secic <haris.secic@gmail.com>
2021-02-23 23:05:30 +0100

    init examples

--- examples/java/src/main/java/io/micronaut/pulsar/example/listeners/PulsarSubscriber.java
+++ examples/java/src/main/java/io/micronaut/pulsar/example/listeners/PulsarSubscriber.java
@@ -1,2 +1,37 @@
-package io.micronaut.pulsar.example.listeners;public interface PulsarSubscriber {
+package io.micronaut.pulsar.example.listeners;
+
+import io.micronaut.core.convert.value.ConvertibleValues;
+import io.micronaut.pulsar.annotation.PulsarConsumer;
+import io.micronaut.pulsar.annotation.PulsarSubscription;
+import io.micronaut.pulsar.example.dto.PulsarMessage;
+import io.micronaut.websocket.WebSocketBroadcaster;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@PulsarSubscription(subscriptionName = "pulsar-test-subscription", subscriptionType = SubscriptionType.Shared)
+public class PulsarSubscriber {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PulsarSubscriber.class);
+    private final WebSocketBroadcaster broadcaster;
+
+    public PulsarSubscriber(WebSocketBroadcaster broadcaster) {
+        this.broadcaster = broadcaster;
+    }
+
+    @PulsarConsumer(topic = "persistent://public/default/messages", consumerName = "shared-consumer-tester")
+    public void messagePrinter(PulsarMessage message) {
+        LOGGER.info("A message was received {}. Sent on {}", message.getMessage(), message.getSent());
+        broadcaster.broadcastAsync(message, t -> isPropperChannel(t.getUriVariables()));
+    }
+
+    private boolean isPropperChannel(ConvertibleValues<?> uriVariables) {
+        if (!"public".equalsIgnoreCase(uriVariables.get("tenant", String.class, null))) {
+            return false;
+        }
+        if (!"default".equalsIgnoreCase(uriVariables.get("namespace", String.class, null))) {
+            return false;
+        }
+        return "messages".equalsIgnoreCase(uriVariables.get("tenant", String.class, null));
+    }
 }
