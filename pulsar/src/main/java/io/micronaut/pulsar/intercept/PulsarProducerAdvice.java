92542b9693bd102a4c4b3c11175bb0f0f35e4033
Haris Secic <haris.secic@gmail.com>
2021-06-25 16:54:30 +0200

    forgotten convert on publisher

--- pulsar/src/main/java/io/micronaut/pulsar/intercept/PulsarProducerAdvice.java
+++ pulsar/src/main/java/io/micronaut/pulsar/intercept/PulsarProducerAdvice.java
@@ -126,7 +126,7 @@ public final class PulsarProducerAdvice implements MethodInterceptor<Object, Obj
             return future;
         }
 
-        return Publishers.fromCompletableFuture(future.toCompletableFuture());
+        return Publishers.convertPublisher(Publishers.fromCompletableFuture(future.toCompletableFuture()), returnType.getType());
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
