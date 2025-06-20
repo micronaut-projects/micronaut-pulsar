ba98f34233eca27b2d7c22e17f2b4e67bb21cd25
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-20 12:00:35 +0200

    Revert "ignore TlsAwareClientTest"
    This reverts commit 23d07ea0953c215b324db07af5b4e3ad7fbe4cf0.

--- test-suite/test-pulsar-module/src/test/groovy/io/micronaut/pulsar/TlsAwareClientTest.groovy
+++ test-suite/test-pulsar-module/src/test/groovy/io/micronaut/pulsar/TlsAwareClientTest.groovy
@@ -31,7 +31,6 @@ import org.apache.pulsar.client.api.PulsarClient
 import org.apache.pulsar.client.api.Reader
 import org.apache.pulsar.client.impl.schema.StringSchema
 import spock.lang.AutoCleanup
-import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
@@ -41,7 +40,6 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReadWriteLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
-@Ignore
 @Stepwise
 class TlsAwareClientTest extends Specification {
 
